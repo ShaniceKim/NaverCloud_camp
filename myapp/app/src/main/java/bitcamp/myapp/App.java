5aa7becb758091feb7b0f9d6ed432017d26bd89c
ShaniceKim <qxzo1@naver.com>
2023-06-13 19:23:02 +0900

    Merge branch 'main' of https://github.com/ShaniceKim/bitcamp-study into main

--- myapp/app/src/main/java/bitcamp/myapp/App.java
+++ myapp/app/src/main/java/bitcamp/myapp/App.java
@@ -1,5 +1,6 @@
 package bitcamp.myapp;
 
+import bitcamp.myapp.handler.BoardHandler;
 import bitcamp.myapp.handler.MemberHandler;
 import bitcamp.util.Prompt;
 
@@ -13,7 +14,7 @@ public class App {
 
     while (true) {
       String menuNo = Prompt.inputString("메인> ");
-      if (menuNo.equals("6")) {
+      if (menuNo.equals("99")) {
         break;
       } else if (menuNo.equals("menu")) {
         printMenu();
@@ -27,6 +28,16 @@ public class App {
         MemberHandler.updateMember();
       } else if (menuNo.equals("5")) {
         MemberHandler.deleteMember();
+      } else if (menuNo.equals("6")) {
+        BoardHandler.inputBoard();
+      } else if (menuNo.equals("7")) {
+        BoardHandler.printBoards();
+      } else if (menuNo.equals("8")) {
+        BoardHandler.viewBoard();
+      } else if (menuNo.equals("9")) {
+        BoardHandler.updateBoard();
+      } else if (menuNo.equals("10")) {
+        BoardHandler.deleteBoard();
       } else {
         System.out.println(menuNo);
       }
@@ -36,19 +47,22 @@ public class App {
   }
 
   static void printMenu() {
-    System.out.println("1. 회원 등록");
-    System.out.println("2. 회원 목록");
-    System.out.println("3. 회원 조회");
-    System.out.println("4. 회원 변경");
-    System.out.println("5. 회원 삭제");
-    System.out.println("6. 종료");
-    System.out.println("");
+    System.out.println("1. 회원등록");
+    System.out.println("2. 회원목록");
+    System.out.println("3. 회원조회");
+    System.out.println("4. 회원변경");
+    System.out.println("5. 회원삭제");
+    System.out.println("6. 게시글등록");
+    System.out.println("7. 게시글목록");
+    System.out.println("8. 게시글조회");
+    System.out.println("9. 게시글변경");
+    System.out.println("10. 게시글삭제");
+    System.out.println("99. 종료");
   }
 
   static void printTitle() {
-    System.out.println("");
-    System.out.println("< 미용실 회원 관리 시스템 >");
-    System.out.println("");
+    System.out.println("나의 목록 관리 시스템");
+    System.out.println("----------------------------------");
   }
 
   static boolean promptContinue() {
