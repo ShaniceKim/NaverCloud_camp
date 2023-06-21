52104737a18ab6ab4a86b6190caec4505135a55a
ShaniceKim <qxzo1@naver.com>
2023-06-21 22:09:31 +0900

    .

--- myapp/app/src/main/java/bitcamp/util/Queue.java
+++ myapp/app/src/main/java/bitcamp/util/Queue.java
@@ -1,8 +1,25 @@
 package bitcamp.util;
 
 public class Queue extends LinkedList {
-  public void offer(Object value) {
 
+  public static void main(String[] args) {
+    Queue q = new Queue();
+    q.offer("홍길동");
+    q.offer("임꺽정");
+    q.offer("유관순");
+    q.offer("안중근");
+    q.offer("윤봉길");
+
+    System.out.println(q.poll());
+    System.out.println(q.poll());
+    System.out.println(q.poll());
+    System.out.println(q.poll());
+    System.out.println(q.poll());
+
+    System.out.println(q.poll());
+  }
+
+  public void offer(Object value) {
     this.add(value);
   }
 
@@ -12,6 +29,4 @@ public class Queue extends LinkedList {
     }
     return this.remove(0);
   }
-
-
 }
